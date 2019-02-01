0cf823373c3c746ba86a8e6a91003074d7b94a43
naufal <ahmednofal@aucegypt.edu>
2019-02-01 15:24:55 +0200

    saving for merging with upstream

--- grr/acauthority/controller.py
+++ grr/acauthority/controller.py
@@ -34,31 +34,87 @@
 # keycloak.KeycloakAdmin.create_client(x)
 
 
+# abstractions
+# keycloak clients are apps generally but they are the server analysts
+# Just to assign to each of them a role
+# Then a user account is managed by the client with the available role
+
+# The server analyst will be the client trying to access the users with certain roles
+# The roles will be just a database of tags, the tags will be analyzed using the controller
+# TODO: Check what would happend when there are non conventional roles, where to check the roles actions
+# TODO: adopt the grr terminology when dealing with clients in keycloak and refer to them as analysts
+# TODO: Figure out a way to be able to keep the connection open with the underlying keycloak server
+# TODO: Problem, the current client : analyst abstraction does not offer a relationship to the user
+# TODO: might be it a good idea to switch for client to actually be the client machine and the user would be the server
+# analyst, but then still we donot have an association between each client and each user and the roles assumed
+# TODO: we can always go back to groups being the client machines in which users are existant and can assume roles
+# TODO: Might it not be a good idea to keep track of the machines and just keep track of the server analysts ids
+
+
+# Flow
+# client -> action -> acauth -> databases -> client role -> approve/deny role ->
+
 import keycloak
-
-class Token:
-    pass
-
+import requests
 class ACAuthority:
+    # Needed because the server apparently kicks us out every few ms
+    def auth(self):
+        self.adminobj = keycloak.keycloak_admin.KeycloakAdmin("http://localhost:8080/auth/", self.admin_user_name, self.admin_password)
+
+    # This should be remove in favor of an initialization and installation script
     def __init__(self):
         # AC Authority initialization
         # It should firstly get access to the admin account of keycloak
-
         self.admin_user_name = "admin"
         self.admin_password = "9437618525"
         # self.adminobj = keycloak.keycloak_admin.KeycloakAdmin("http://localhost:8080/auth/", self.admin_user_name, self.admin_password)
-        self.adminobj = keycloak.keycloak_admin.KeycloakAdmin("http://localhost:8080/auth/", self.admin_user_name, self.admin_password)
-        pass
+        self.auth()
+
+    def request_action(self,request):
+        self.auth()
+        return request.action
+
+    def request_analyst(self, request):
+        self.auth()
+        return request.analyst
+
+    def request_client_machine(self, request):
+        self.auth()
+        return request.request_machine
+
+    def lookup_role(self, request):
+        self.auth()
+        analyst = self.request_analyst(request)
+        return self.adminobj.get_client_roles(analyst)
+
+    def lookup_action(self, request):
+        self.auth()
+        action = self.request_action(request)
+
+    def identity_as_db_entry(self,identity):
+        self.auth()
+
     def get_identity(self, request):
-        self.adminobj.get_client(request.identity)
+        self.auth()
+        current_users = self.adminobj.get_users()
+        db_entry = self.identity_as_db_entry(self.request)
 
     def facilitate_roles(self, request):
+        self.auth()
         identity = self.get_identity(request);
-    def identity_roles(server_IP):
+
+    # DEPRECATED or is it ?
+    def identity_roles(self,server_IP):
+        self.auth()
+        realm_groups = self.adminobj.get_groups()
+        # The single group is a client in our definition and the users in the group are the server analysts
+        # subscribed to roles to be used analyzing the client machine
+
+        for group in realm_groups:
+            pass
         pass
 
-    # cross checks the access requested with the supplied token
-    def access_token_valid(token, access):
-        pass
+
+
 
 
